c2956acbffa61ce901012be76c6510fddd3becc0
jamesbenedict <jabenedi@indiana.edu>
2016-10-09 21:26:38 -0400

    ipad testing

--- js/loadMore.js
+++ js/loadMore.js
@@ -12,7 +12,7 @@ $('#viewLess').click(function() {
    
     $('#extra-work').slideUp(200);
     $('html,body').animate({
-            scrollTop: $(this).offset().top-2000
+            scrollTop: $(this).offset().top-1200
         }, 1000);
     $('#viewMore').toggle();
     $('#viewLess').toggle() ;           
