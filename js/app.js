a496112a1c1e56753e5bd0c405c7fd0b335a2966
JamesBenedict <jamesbenedict0@gmail.com>
2023-08-17 14:00:52 -0400

    removed logs

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 async function loadData() {
-  console.time('loadData')
+  // console.time('loadData')
   const url="https://docs.google.com/spreadsheets/d/e/2PACX-1vQTD8mBr_86sDT3zVpt7cvpiUqxU10EXuDXejpUNR4PKR9xTHDY15f-jSyeqrpNdTvULU2YAxCBbrm7/pub?output=tsv";
 
   let data = ''
   xmlhttp=new XMLHttpRequest();
   xmlhttp.onreadystatechange = async function() {
     if(xmlhttp.readyState == 4 && xmlhttp.status==200){
-      console.time('hi')
+      // console.time('hi')
       data = await xmlhttp.responseText
       buildDom(data)
-      console.timeEnd('hi')
+      // console.timeEnd('hi')
     }
   };
   xmlhttp.open("GET",url,true);
   xmlhttp.send(null);
-  console.timeEnd('loadData')
+  // console.timeEnd('loadData')
   return data
 }
 
 async function buildDom(data){
-  console.time('buildDome')
+  // console.time('buildDome')
   const rows = data.split('\n')
   const cells = rows.map((d) => {
     return d.split('\t')
@@ -51,14 +51,10 @@ async function buildDom(data){
   <ul>${all}</ul>
   `
   document.getElementById('work-container').innerHTML = markup
-  console.timeEnd('buildDome')
+  // console.timeEnd('buildDome')
 }
 
-// import {handlebars} from './handlebars-v4.0.5.js'
-// console.log(handlebars)
-// const Handlebars = require("handlebars");
-// const template = Handlebars.compile("Name: {{name}}");
-// console.log(template({ name: "Nils" }));
+
 
 
 
